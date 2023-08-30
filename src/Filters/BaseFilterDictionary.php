6742f94ae47f4b4ac253caced3f70cf929f51a08
alexander <sanyaerin@mail.ru>
2023-08-30 22:45:40 +0300

    refactoring

--- src/Filters/BaseFilterDictionary.php
+++ src/Filters/BaseFilterDictionary.php
@@ -6,10 +6,13 @@ namespace IWD\Templator\Filters;
 
 class BaseFilterDictionary
 {
-    public const URLIZE = UrlizeFilter::URLIZE;
     public const CAMELIZE = CamelizeFilter::CAMELIZE;
     public const CAPITALIZE = CapitalizeFilter::CAPITALIZE;
-    #todo:)
-    # - дописать тут
-    # - доделать линтеры зелеными
+    public const CLASSIFY = ClassifyFilter::CLASSIFY;
+    public const CONSTANTIZE = ConstantizeFilter::CONSTANTIZE;
+    public const KEBAB = KebabizeFilter::KEBAB;
+    public const PLURALIZE = PluralizeFilter::PLURALIZE;
+    public const SINGULARIZE = SingularizeFilter::SINGULARIZE;
+    public const TABLEIZE = TableizeFilter::TABLEIZE;
+    public const URLIZE = UrlizeFilter::URLIZE;
 }
